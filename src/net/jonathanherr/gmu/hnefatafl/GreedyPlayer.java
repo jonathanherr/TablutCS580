258bada787f92d289e19ce3bd10c2764a7936e26
Jonathan <jrcedric@gmail.com>
2014-05-01 21:31:33 -0400

    greedy player

---
+++ src/net/jonathanherr/gmu/hnefatafl/GreedyPlayer.java
@@ -0,0 +1,18 @@
+package net.jonathanherr.gmu.hnefatafl;
+
+import java.util.ArrayList;
+
+public class GreedyPlayer extends Player {
+
+	public GreedyPlayer(Hnefatafl game, ArrayList<Piece> pieces) {
+		super(game, pieces, "Greedy");
+		
+	}
+	public Move turn(int turnnum) {
+		
+		
+		return null;
+		
+	}
+
+}
